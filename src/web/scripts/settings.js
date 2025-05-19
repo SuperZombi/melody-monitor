de6eade994dbbc788cd4c8cfba6b169e43b0451f
Super Zombi <super.zombi.yt@gmail.com>
2025-05-19 19:10:49 +0300

    fix some ui bugs in settings

--- src/web/scripts/settings.js
+++ src/web/scripts/settings.js
@@ -81,11 +81,11 @@ async function buildMods(){
 
 function Setting(data){
 	let div = document.createElement("div")
-	div.className = "row"
+	div.className = "row align-items-center"
 	div.innerHTML = `
 		<label class="col-sm-4 col-form-label">${data.label}</label>
-		<div class="col d-flex">
-			<div class="input-group h-100 align-items-center"></div>
+		<div class="col">
+			<div class="input-group"></div>
 		</div>
 	`
 	let input_area = div.querySelector(".input-group")
@@ -151,7 +151,7 @@ function Setting(data){
 
 	if (data.promt){
 		let span = document.createElement("span")
-		span.className = "input-group-text help h-100"
+		span.className = "input-group-text help"
 		span.title = data.promt
 		span.innerHTML = `<i class="bi bi-question-circle"></i>`
 		input_area.appendChild(span)
