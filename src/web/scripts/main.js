318fde7ce2631328a5a7cabac2fb88ea30c91c8e
Super Zombi <super.zombi.yt@gmail.com>
2025-05-21 20:18:04 +0300

    Add: time

--- src/web/scripts/main.js
+++ src/web/scripts/main.js
@@ -14,6 +14,7 @@ window.onload=_=>{
         eel.get_media_info()
     }, 1000)
 }
+var timeRenderer;
 
 eel.expose(update_media_info);
 function update_media_info(info) {
@@ -23,6 +24,7 @@ function update_media_info(info) {
         document.querySelector(".track-info").classList.add("active")
         document.querySelector("#trackName").innerHTML = info.title
         document.querySelector("#trackAuthor").innerHTML = info.artist
+        if (timeRenderer){clearTimeout(timeRenderer)}
         checkMarquee()
 
         if (info.thumbnail){
@@ -32,19 +34,33 @@ function update_media_info(info) {
             document.querySelector("#trackImage").src = document.querySelector("#trackImage").getAttribute("default")
         }
         if (info.total > 0){
-            document.querySelector(".progress-bar").style.display = "block"
+            document.querySelector(".progress-bar").classList.remove("hide")
+            document.querySelector(".time").classList.remove("hide")
             document.querySelector(".progress-bar .progress").style.transition = ""
             let percent = info.current * 100 / info.total;
             document.querySelector(".progress-bar .progress").style.width = `${percent}%`
-            if (info.status == "PLAYING"){
-                let remain = info.total - info.current;
-                setTimeout(_=>{
-                    document.querySelector(".progress-bar .progress").style.transition = `${remain}s linear`
-                    document.querySelector(".progress-bar .progress").style.width = "100%"
-                }, 10)
+
+            document.querySelector(".time-current").innerHTML = formatTime(info.current)
+            document.querySelector(".time-total").innerHTML = formatTime(info.total)
+
+            let remain = info.total - info.current;
+            setTimeout(_=>{
+                document.querySelector(".progress-bar .progress").style.transition = `${remain}s linear`
+                document.querySelector(".progress-bar .progress").style.width = "100%"
+            }, 10)
+
+            let targetTime = getUnixTime() + remain
+            function renderTime(){
+                let timeRemaining = Math.max(0, targetTime - getUnixTime())
+                let currentTime = info.total - timeRemaining
+                document.querySelector(".time-current").innerHTML = formatTime(currentTime)
+                if (timeRemaining <= 0){return}
+                timeRenderer = setTimeout(renderTime, 1000)
             }
+            timeRenderer = setTimeout(renderTime, 1000)
         } else{
-            document.querySelector(".progress-bar").style.display = "none"
+            document.querySelector(".progress-bar").classList.add("hide")
+            document.querySelector(".time").classList.add("hide")
         }
     }
     else{
@@ -84,4 +100,22 @@ async function load_mods(){
 eel.expose(update_url);
 function update_url(url) {
     window.location.href = url;
+}
+function getUnixTime(){
+    return Math.floor(Date.now() / 1000)
+}
+function formatTime(seconds) {
+    let h = Math.floor(seconds / 3600);
+    let m = Math.floor((seconds % 3600) / 60);
+    let s = seconds % 60;
+
+    let mm = String(m).padStart(2, '0');
+    let ss = String(s).padStart(2, '0');
+
+    if (h > 0) {
+        let hh = String(h).padStart(2, '0');
+        return `${hh}:${mm}:${ss}`;
+    } else {
+        return `${mm}:${ss}`;
+    }
 }
